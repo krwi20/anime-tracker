cb16a4e42363a4d55b767a3535b399c6536b0ba5
krwi20 <102562042+krwi20@users.noreply.github.com>
2023-11-09 01:56:03 +0000

    Login + Register Functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,28 +1,51 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+	const [formData, setFormData] = useState({});
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				"http://localhost:3001/api/auth/register",
+				formData
+			);
+			console.log(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className='bg-[#171717] border-t-white border-t-2 min-h-[calc(100vh-64px)] text-white flex justify-center items-center'>
 			<div className='bg-[#202020] rounded-lg'>
 				<div className='m-20'>
-					<form className='flex flex-col gap-4'>
+					<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
 						<input
 							type='text'
 							placeholder='Username'
 							id='username'
 							className='bg-[#202020] outline-none rounded-lg border-2 border-[#444444] h-8 focus:border-[#da0037] text-[#ededed] px-3'
+							onChange={handleChange}
 						/>
 						<input
 							type='email'
 							placeholder='Email'
 							id='email'
 							className='bg-[#202020] outline-none rounded-lg border-2 border-[#444444] h-8 focus:border-[#da0037] text-[#ededed] px-3'
+							onChange={handleChange}
 						/>
 						<input
 							type='password'
 							placeholder='Password'
 							id='password'
 							className='bg-[#202020] outline-none rounded-lg border-2 border-[#444444] h-8 focus:border-[#da0037] text-[#ededed] px-3'
+							onChange={handleChange}
 						/>
 						<button className='bg-[#202020] border-2 border-[#da0037] rounded-lg text-[#ededed] h-8 cursor-pointer hover:bg-[#da0037]'>
 							Sign Up
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.js";
 
@@ -17,6 +18,12 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
 
 app.listen(3001, () => {
 	console.log("Server listening on port 3001");
